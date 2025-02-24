4469222e837954a8bd526e79a9414eac4f08e070
RogerBallester <21750735a@iespoblenou.org>
2025-02-24 17:58:04 +0100

    sfeeefa

---
+++ tes-oficina/oficina.gd
@@ -0,0 +1,63 @@
+extends Node2D
+var Izq:bool = false
+var Der:bool = false
+var puertaDer:bool = false
+var puertaIzq:bool = false
+var barrader:bool = false
+var barraizq:bool = false
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+func movCamara(Iqz:bool, Der:bool):
+	if Iqz && $Camera.position.x > -127:
+		$Camera.position.x -= 20
+	if Der && $Camera.position.x < 133:
+		$Camera.position.x += 20 	
+
+func _process(delta: float) -> void:
+	movCamara(Izq,Der)
+	pass
+
+func _on_izq_mouse_entered() -> void:
+	Izq = true
+	pass # Replace with function body.
+	
+
+func _on_izq_mouse_exited() -> void:
+	Izq = false
+	pass # Replace with function body.
+
+func _on_der_mouse_entered() -> void:
+	Der = true
+	pass # Replace with function body.
+
+func _on_der_mouse_exited() -> void:
+	Der = false
+	pass # Replace with function body.
+
+func _on_botonizq_pressed() -> void:
+	if not puertaIzq:
+		$Botones/Izq/btnizq.play("encender")
+		$Puertas/Izq/AnimatedSprite2D.play("default")
+		puertaIzq = true
+	else:
+		$Botones/Izq/btnizq.play("apagado")
+		$Puertas/Izq/AnimatedSprite2D.play_backwards("default")
+		puertaIzq = false
+		
+	pass # Replace with function body.
+	
+
+func _on_botonder_pressed() -> void:
+	if not puertaDer:
+		$Botones/Der/btnder.play("encender")
+		$Puertas/Der/AnimatedSprite2D.play("default")
+		$"Botones/Der/Barra electricidad/AnimatedSprite2D".play("default")
+		puertaDer = true
+	else:
+		$Botones/Der/btnder.play("apagado")
+		$Puertas/Der/AnimatedSprite2D.play_backwards("default")
+		$"Botones/Der/Barra electricidad/AnimatedSprite2D".play_backwards("default")
+		puertaDer = false
+	pass # Replace with function body.
